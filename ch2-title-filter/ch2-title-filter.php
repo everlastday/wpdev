894aff84b098dc910f61bcf8452a3881495e0d25
WestSIDe <eastside@bigmir.net>
2013-10-21 20:02:08 +0300

    ch2 title plugin complete

--- ch2-title-filter/ch2-title-filter.php
+++ ch2-title-filter/ch2-title-filter.php
@@ -1,8 +1,37 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: WestSIDe
- * Date: 21.10.13
- * Time: 18:37
- * To change this template use File | Settings | File Templates.
- */
+  /*
+  Plugin Name: Chapter 2 – Title Filter
+  Plugin URI:
+  Description: Declares a plugin that will be visible in the
+  WordPress admin interface
+  Version: 1.0
+  Author: WestSIDe
+  Author URI: http://ylefebvre.ca
+  License: GPLv2
+  */
+
+
+  add_filter( 'wp_title', 'ch2tf_title_filter' );
+
+
+  function ch2tf_title_filter ( $title ) {
+    if ( is_front_page() )
+      $new_title = 'Front Page >> ';
+    elseif ( get_post_type() == 'page' )
+      $new_title = 'Page >> ';
+    elseif(get_post_type() == 'post')
+      $new_title = 'Post >> ';
+
+    // Append previous title to title prefix
+
+    if ( isset($new_title)) {
+      $new_title .= $title;
+      // Return new complete title to be displayed
+      return $new_title;
+      //
+    } else {
+      return $title;
+    }
+
+
+  }
